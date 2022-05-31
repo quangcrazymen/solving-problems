39a13f3b2614b407c58999e06e0a6417b20a0bb8
Quang <quangcrazymen@gmail.com>
2022-05-31 11:20:52 +0700

    Solved 392ms - 51.9 MB

--- leet-code/Add-Two-Numbers.js
+++ leet-code/Add-Two-Numbers.js
@@ -5,13 +5,14 @@ function ListNode(val=undefined, next=null) {
     this.next = (next === undefined ? null : next)
 }
 
-const a = new ListNode(3, null)
-const b = new ListNode(4, a)
-const c = new ListNode(2, b)
+const a = new ListNode(9, null)
+const b = new ListNode(9, a)
+const c = new ListNode(9, b)
 
-const d = new ListNode(4, null)
-const e = new ListNode(6, d)
-const f = new ListNode(5, e)
+const d = new ListNode(9, null)
+const e = new ListNode(9, d)
+const f = new ListNode(9, e)
+const g = new ListNode(9, f)
 
 //Nice try but not good enough
 let addTwoNumbers = function (l1, l2) {
@@ -54,16 +55,53 @@ let addTwoNumbers = function (l1, l2) {
     console.log(node)
     return node
 }
+//Adding to number version 2: c, f
+let addTwoNumbers2 =function(l1,l2){
+    let result = []
+    let remain = 0
+    while(l1!==null || l2!==null){
+        let a = l1 !== null ? l1.val:0
+        let b = l2 !== null ? l2.val:0
+        let sum = a + b 
+        if(sum +remain >= 10){
+            newValue = (sum+remain) % 10  
+            remain = 1 
+        }
+        else{
+            newValue = sum +remain
+            remain = 0
+        }
+        result.push(newValue)
+        l1!==null? l1=l1.next:null
+        l2!==null? l2=l2.next:null
+    } 
+    remain === 1? result.push(1):undefined
+    console.log(result)
+    let node = new ListNode()
+    let i = result.length
+    function chainNode(node,result,i){
+        if(i===0) return 
+        node.val = result[i-1]
+        let nextNode = new ListNode(undefined,null)
+        if (i!==1) node.next =nextNode;
+        chainNode(nextNode,result, i-1)
+    }
+    chainNode(node,result.reverse(),i)
+    console.log(node)
+    return node
+}
+addTwoNumbers2(c,g)
+
 // console.log(1+2)
 // console.log(addTwoNumbers(1,2))
 // console.log([1,2,4].reverse())
 // console.log([1,2,4].splice(0,1))
 
-addTwoNumbers(f,c)
+// addTwoNumbers(f,c)
 
 
 //Test join function
-console.log(typeof(["1","2","3"].join("")))
-const aString = ["1","2","3"].join("")
-console.log(typeof(Number(aString)))
+//console.log(typeof(["1","2","3"].join("")))
+//const aString = ["1","2","3"].join("")
+// console.log(typeof(Number(aString)))
 
