24091c310b58ee5b60f0d21a7b59d8f7c0cfcf9d
Quang <quangcrazymen@gmail.com>
2022-09-10 10:05:55 +0700

    attempt to solve A permutation

---
+++ Hackerank/Absolute-permutation.py
@@ -0,0 +1,24 @@
+from xml.etree.ElementTree import ElementTree
+
+
+n = 4
+k = 2 
+
+# maybe try to use recursion
+
+def absolutePermutation(n,k):
+    elements = [i for i in range(1,n+1)]
+    for i in range(0,n-1):
+        for j in range(i+1,n):
+            temp = []
+            for e in elements:
+                if e == elements[i] | e==elements[j]: temp.append(0)
+                else: temp.append(e)
+            temp[j]=elements[i]
+            temp[i]=elements[j]
+            print(temp)
+
+
+absolutePermutation(n,k)
+print('original')
+print([i for i in range(1,n+1)])
